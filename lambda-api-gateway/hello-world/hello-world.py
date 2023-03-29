b5654f1b724d473bdf9edbf6009d13042f8ba81f
He.Camille <camille.he@outlook.com>
2023-03-29 21:49:28 +0800

    feat: creat api-gateway

--- lambda-api-gateway/hello-world/hello-world.py
+++ lambda-api-gateway/hello-world/hello-world.py
@@ -3,7 +3,11 @@ import json
 
 def lambda_handler(event, context):
     print(event)
+    name = "World"
+    if event.get("queryStringParameters") and event["queryStringParameters"]["Name"]:
+        name = event["queryStringParameters"]["Name"]
+
     return {
         "statusCode": 200,
-        "message": json.dumps("Hello from Lambda!")
+        "message": json.dumps(f"Hello from {name}!")
     }
