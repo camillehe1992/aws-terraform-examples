9fd2744a8bbf120ae90a1e029a52afb2696fa703
camillehe1992 <camille.he@outlook.com>
2023-11-14 16:38:13 +0800

    feat: increased lambda timeout to 60s

--- ecs-cluster-on-ec2/02_lambda.tf
+++ ecs-cluster-on-ec2/02_lambda.tf
@@ -3,8 +3,10 @@ resource "aws_lambda_function" "drain_ecs_tasks" {
   function_name    = "${var.env}-${var.nickname}-drain-ecs-tasks"
   description      = "Gracefully drain ECS tasks from EC2 instances before the instances are terminated by autoscaling."
   role             = module.instance_terminating_lambda_execution_role.iam_role.arn
-  handler          = "index.lambda_handler"
+  handler          = "drain-ecs-tasks.lambda_handler"
+  memory_size      = 128
   runtime          = "python3.9"
+  timeout          = 60
   filename         = "drain-ecs-tasks.zip"
   source_code_hash = data.archive_file.drain_ecs_tasks.output_base64sha256
 
@@ -16,6 +18,14 @@ resource "aws_lambda_function" "drain_ecs_tasks" {
 
   tags = var.tags
 }
+
+# https://registry.terraform.io/providers/hashicorp/aws/5.0.0/docs/resources/cloudwatch_log_group
+resource "aws_cloudwatch_log_group" "instance_terminating" {
+  name              = "/aws/lambda/${var.env}-${var.nickname}-drain-ecs-tasks"
+  retention_in_days = var.retention_in_days
+  tags              = var.tags
+}
+
 
 # https://registry.terraform.io/providers/hashicorp/aws/5.0.0/docs/resources/lambda_permission
 resource "aws_lambda_permission" "instance_terminating" {
